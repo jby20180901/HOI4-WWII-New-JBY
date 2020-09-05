7c307f50cd127152fce484816516097134c421c3
jby20180901 <2123189960@qq.com>
2020-09-05 17:16:20 +0800

    common

---
+++ common/defines/01_defines.lua
@@ -0,0 +1,11 @@
+NDefines.NCountry.BASE_STABILITY_WAR_FACTOR = -0.2			-- Default stability war factor
+NDefines.NCountry.BASE_STABILITY_PARTY_POPULARITY_FACTOR = 0.2	-- Default stability rulling party popularity factor
+NDefines.NCountry.MIN_STABILITY = 0
+NDefines.NCountry.MAX_STABILITY = 2
+NDefines.NCountry.MIN_WAR_SUPPORT = -1
+NDefines.NCountry.MAX_WAR_SUPPORT = 1
+NDefines.NCountryDAYS_OF_WAR_BEFORE_SURRENDER = 0
+NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 15
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50
+NDefines.NDiplomacy.TENSION_CAPITULATE = 0
+NDefines.NMilitary.BASE_COMBAT_WIDTH = 120
---
+++ common/defines/te_defines.lua
@@ -0,0 +1,26 @@
+NDefines.NGame.END_DATE = "2000.1.1.1" --游戏结束时间
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 50 --有玩家延迟x天后减速
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 75 --有玩家延迟x天后暂停
+
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.03 --人口增速*2
+NDefines.NCountry.POLITICAL_POWER_CAP = 5000 --政治力量槽储存最大值/2*5
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 200 --指挥点槽储存最大值*2
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.1 --特种兵百分比*2
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 48 --特种兵营基础数量*2
+NDefines.NCountry.BASE_FUEL_CAPACITY = 100000 --基础燃料容量*2
+
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 50 --科技槽储存最大值/3*5
+
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 2000 --陆军经验槽储存最大值*4
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 2000 --海军经验槽储存最大值*4
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 2000 --空军经验槽储存最大值*4
+NDefines.NMilitary.UNIT_EXP_LEVELS = { 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.85 } --单位经验分级*2
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2 --部署最高等级*2
+NDefines.NMilitary.TRAINING_MAX_LEVEL = 4 --训练最高等级*2
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 2 --基本等级*2
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.1 --等级奖励/5*2
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2 --模板支援连宽度*2
+NDefines.NMilitary.UNIT_LEADER_INITIAL_TRAIT_SLOT = { 2.0, 1.0, 2.0 } --{元帅,将军,海军}初始技能槽+1
+NDefines.NMilitary.UNIT_LEADER_TRAIT_SLOT_PER_LEVEL = { 0.75, 0.75, 0.75 } --{元帅,将军,海军}升级技能槽/2*3
+
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30 --国策槽储存最大值*3
